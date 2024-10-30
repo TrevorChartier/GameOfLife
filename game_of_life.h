2e82dbeeb1c60e629d5337ad25eb3e3cd3ee6952
Trevor Chartier <trevor.chartier@colostate.edu>
2024-10-30 11:48:07 -0600

    Update Header File With New Methods and Docstrings

--- game_of_life.h
+++ game_of_life.h
@@ -56,21 +56,25 @@ public:
   GameOfLife() = delete;
 
   /**
+   * ~GameOfLife()
    * Default destructor for GameOfLife objects
    */
   ~GameOfLife() = default;
 
   /**
+   * GameOfLife(const GameOfLife &other)
    * Default copy constructor for GameOfLife objects
    */
   GameOfLife(const GameOfLife &other) = default;
 
   /**
+   * GameOfLife &operator=(const GameOfLife &other)
    * Default copy assignment operator for GameOfLife objects
    */
   GameOfLife &operator=(const GameOfLife &other) = default;
 
   /**
+   * GameOfLife(std::string filename)
    * File constructor, construct a GameOfLife object gameboard from an input
    * file
    *
@@ -80,7 +84,8 @@ public:
   GameOfLife(std::string filename);
 
   /**
-   * pregeneration constructor, construct a GameOfLife object gameboard from an
+   * GameOfLife(std::string filename, int generation_count)
+   * Pre-generation constructor, construct a GameOfLife object gameboard from an
    * input file and set a pre-generation command
    *
    * @param filename The filepath for the .txt file containing
@@ -91,7 +96,8 @@ public:
   GameOfLife(std::string filename, int generation_count);
 
   /**
-   * character constructor, construct a GameOfLife object gameboard from an
+   * GameOfLife(std::string filename, char live_cell, char dead_cell)
+   * Character constructor, construct a GameOfLife object gameboard from an
    * input file with custom cell characters
    *
    * @param filename The filepath for the .txt file containing
@@ -102,9 +108,10 @@ public:
   GameOfLife(std::string filename, char live_cell, char dead_cell);
 
   /**
-   * Full Constructor, construct a GameOfLife object
-   * gameboard from an input file with custom cell characters and
-   * a pre-generation coommand
+   * GameOfLife(std::string filename, char live_cell, char dead_cell, int
+   * generationCount) Full Constructor, construct a GameOfLife object gameboard
+   * from an input file with custom cell characters and a pre-generation
+   * coommand
    *
    * @param filename The filepath for the .txt file containing
    * an initial board state
@@ -117,13 +124,15 @@ public:
              int generationCount);
 
   /**
-   * GetGenerations retreives the number of generation calculations that the
+   * GetGenerations()
+   * Retreives the number of generation calculations that the
    * GameOfLife object has gone through
    */
   int GetGenerations() const { return generations_; }
 
   /**
-   * SetLiveCell changes the character for the Live Cell
+   * SetLiveCell(char live_cell)
+   * Changes the character for the Live Cell
    * It cannot be set to whatever the current Dead Cell character is.
    *
    * @throws Runtime Error if live cell character is attempted to be set
@@ -134,7 +143,8 @@ public:
   void SetLiveCell(char live_cell);
 
   /**
-   * SetDeadCell changes the character for the Dead Cell
+   * SetDeadCell(char dead_cell)
+   * Changes the character for the Dead Cell
    * It cannot be set to whatever the current Live Cell character is.
    *
    * @throws Runtime Error if dead cell character is attempted to be set
@@ -145,20 +155,84 @@ public:
   void SetDeadCell(char dead_cell);
 
   /**
-   *
+   * operator+(int N)
+   * The "+" operator for GameOfLife objects returns what the
+   * state of the game board would be in int N generations
    */
-  GameOfLife operator+(int) const;
+  GameOfLife operator+(int N) const;
 
   /**
-   * NextNGen calculates next generation of the game of life
-   * n times
+   * operator+=(int)
+   * The "+=" operator for GameOfLife objects calculates the
+   * next int N generations on the current object and returns itself
+   */
+  GameOfLife &operator+=(int);
+
+  /**
+   * operator++()
+   * The preincrement operator calculates next generation on the current object
+   * and returns itself
+   */
+  GameOfLife &operator++();
+
+  /**
+   * operator++(int)
+   * The postincrement operator calculates the next generation on the current
+   * object but returns a version of itself before the increment
+   */
+  GameOfLife operator++(int);
+
+  /**
+   * operator==(const GameOfLife &)
+   * The  "==" operator compares if the proportion of living cells in two
+   * GameOfLife objects are within 0.5%
+   */
+  bool operator==(const GameOfLife &) const;
+
+  /**
+   * operator<(const GameOfLife &)
+   * The "<" operator compares if the proportion of living cells in current
+   * GameOfLife object is strictly less than another GameOfLife object
+   */
+  bool operator<(const GameOfLife &) const;
+
+  /**
+   * operator>(const GameOfLife &)
+   * The ">" operator compares if the proportion of living cells in current
+   * GameOfLife object is strictly greater than another GameOfLife object
+   */
+  bool operator>(const GameOfLife &) const;
+
+  /**
+   * operator<=(const GameOfLife &)
+   * The "<=" operator compares if the proportion of living cells in current
+   * GameOfLife object is less than or within 0.5% of another GameOfLife object
+   */
+  bool operator<=(const GameOfLife &) const;
+
+  /**
+   * operator>=(const GameOfLife &)
+   * The ">=" operator compares if the proportion of living cells in current
+   * GameOfLife object is greater than or within 0.5% of another GameOfLife
+   * object
+   */
+  bool operator>=(const GameOfLife &) const;
+
+  /**
+   * NextNGen()
+   * @brief Calculates the next N generations of the current GameOfLife object
+   * and sets this to be the new state of the object. Utilizes wrap around to
+   * guarantee each cell has 8 neighbors. n times
    */
   void NextNGen(int n);
 
   /**
-   * @brief NextGen calculates "next" generation of the game of life
+   * NextGen()
+   * @brief Calculates next generation of the GameOfLife object and sets this to
+   * be the new state of the object. Utilizes wrap around to guarantee each cell
+   * has 8 neighbors.
    *
-   * NextGen calculates whether each cell will be dead or alive in the
+   * Calculates whether each cell will be dead or alive in the
    * next generation based on the number of alive neighbors it has
    * in the current generation.
    */
@@ -166,8 +240,9 @@ public:
 
 private:
   /**
-   * AliveNextGen determines whether a specified index will be alive
-   * in the next generation
+   * AliveNextGen(size_t index)
+   * Determines whether the specified index in the game board string will be
+   * alive in the next generation
    *
    * @return bool, true if index will be alive next generation,
    * otherwise false
@@ -175,7 +250,8 @@ private:
   bool AliveNextGen(size_t index);
 
   /**
-   * CalcNumLiveNeighbors determines how many neighbors a given cell has that
+   * CalcNumLiveNeighbors(size_t index)
+   * Determines how many neighbors the cell at the given index has that
    * are alive in the current generation
    *
    * @return int, the number of live neighbors
@@ -183,7 +259,8 @@ private:
   int CalcNumLiveNeighbors(size_t index);
 
   /**
-   * GetNeighborIndices finds the indices in _current string that are
+   * GetNeighborIndices(size_t index)
+   * Finds the indices in _current string that are
    * neighbors to the cell specified by index
    *
    * @return std::array<int,8> an array of the index for each
@@ -192,8 +269,9 @@ private:
   std::array<size_t, 8> GetNeighborIndices(size_t index);
 
   /**
-   * NumAlive, given an array of indices, determine how many
-   * of the cells at those indices in the game board are alive
+   * NumAlive(std::array<size_t, 8> cell_indices)
+   * Given an array of indices, determine how many of the cells at those indices
+   * in the game board are alive
    *
    * @param cell_indices: an array of index values in the current_ string
    *
@@ -203,7 +281,8 @@ private:
   int NumAlive(std::array<size_t, 8> cell_indices);
 
   /**
-   * Alive determines if the cell at the specified index is alive
+   * Alive(size_t index)
+   * Determines if the cell at the specified index is alive
    * in current generation in the gameboard
    *
    * @return boolean: true if cell is alive, otherwise false
@@ -211,19 +290,22 @@ private:
   bool Alive(size_t index);
 
   /**
-   * ConvertTo1d converts a row,col coordinate pair into the proper location in
+   * ConvertTo1d(int row, int col))
+   * Converts a row,col coordinate pair into the proper location in
    * a one-dimensional representation of the matrix
    */
   size_t ConvertTo1D(int row, int col);
 
   /**
-   * ConvertTo2D converts an index in a 1 dimensional representation of the
+   * ConvertTo2D(size_t index)
+   * Converts an index in a 1 dimensional representation of the
    * matrix into the correct row,col coordinate in the matrix
    */
   std::pair<size_t, size_t> ConvertTo2D(size_t index);
 
   /**
-   * IncrementCol finds the index of the column directly after the current
+   * IncrementCol(int col)
+   * Finds the index of the column directly after the current
    * column, wrapping around the matrix if necessary
    *
    * @param col current column index
@@ -231,7 +313,8 @@ private:
   int IncrementCol(int col);
 
   /**
-   * DecrementCol finds the index of the column directly preceding the current
+   * DecrementCol(int col)
+   * Finds the index of the column directly preceding the current
    * column, wrapping around the matrix if necessary
    *
    * @param col current column index
@@ -239,7 +322,8 @@ private:
   int DecrementCol(int col);
 
   /**
-   * IncrementRow finds the index of the row directly after the current row,
+   * IncrementRow(int row)
+   * Finds the index of the row directly after the current row,
    * wrapping around the matrix if necessary
    *
    * @param col current row index
@@ -247,7 +331,8 @@ private:
   int IncrementRow(int row);
 
   /**
-   * DecrementRow finds the index of the row directly preceding the current row,
+   * DecrementRow()
+   * Finds the index of the row directly preceding the current row,
    * wrapping around the matrix if necessary
    *
    * @param col current column index
