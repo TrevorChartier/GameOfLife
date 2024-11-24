a252e8e32d1cde67cfdde7df8dc4475b79d09415
Trevor Chartier <trevor.chartier@colostate.edu>
2024-11-23 18:53:41 -0700

    .gitignore

--- test.cpp
+++ test.cpp
@@ -8,8 +8,11 @@ int main(int argc, char const *argv[])
 {
     GameOfLife game("starter.txt");
     cout << game;
-    game.NextGen();
-    cout << ++game;
-	cout << game;
+    game += 105;
+    game.SetLiveCell('L');
+    cout << game;
+    game -=100;
+    cout << game;
+    game -=1;
     return 0;
 }
